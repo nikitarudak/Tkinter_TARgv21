f8cedb2ed09ff7781e91dfad426dd4fbde8a2afa
Nikita Rudak <nikitarudak07@gmail.com>
2021-12-07 11:32:26 +0200

    .

--- Tkinter_TARgv21/Tkinter_TARgv21.py
+++ Tkinter_TARgv21/Tkinter_TARgv21.py
@@ -1,4 +1,5 @@
-from tkinter import*
+from tkinter import *
+from tkinter import ttk
 klik=0
 def klikker(event):
     global klik
@@ -17,14 +18,63 @@ def text_to_lbl(event):
     text=ent.get()
     lbl.configure(text=text)
     ent.delete(0,END)
+
 def valik():
     val=str(var.get())+", "
     ent.insert(END, var+", ")
+
+def ava_pilt(ind:int):
+    global tabs
+    tabs.select(ind)
+
 aken=Tk()
 aken.title("Akna nimetus")
 aken.geometry("600x400")
 
+Menu=Menu(aken)
+aken.config(menu=Menu)
+m1=Menu(Menu)
+Menu.add_cascade(label="Tabs", menu=m1)
+m1.add_command(label="Tab1",acceleration="Command+A", command=lambda:ava_pilt(0))
+m1.add_command(label="Tab2", command=lambda:uus_aken(1))
+m1.add_command(label="Tab3", command=lambda:uus_aken(2))
+m1.add_separator()
+
+
+def uus_aken():
+    uusaken=Toplevel()
+    tabs=ttk.Notebook(uusaken)
+    texts=["1.gif","2.gif","3.gif"]
+
+    tab1=Frame(tabs)
+    img1=PhotoImage(file=texts[0])
+    tabs.add(tab1,text=texts[0])
+    can1=Canvas(tab1,height=1000,width=1000,bg="red")
+    can1.create_image(0,0,image=img1,anchor=NW)
+    can1.pack()
+
+    tab2=Frame(tabs)
+    img2=PhotoImage(file=texts[1])
+    tabs.add(tab2,text=texts[1])
+    can2=Canvas(tab2,height=1000,width=1000,bg="red")
+    can2.create_image(0,0,image=img2,anchor=NW)
+    can2.pack()
+
+    tab3=Frame(tabs)
+    img3=PhotoImage(file=texts[2])
+    tabs.add(tab3,text=texts[2])
+    can3=Canvas(tab3,height=1000,width=1000,bg="red")
+    can3.create_image(0,0,image=img3,anchor=NW)
+    can3.pack()
+
+    tabs.add(tab1,text=texts[0])
+    tabs.add(tab2,text=texts[1]) 
+    tabs.add(tab3,text=texts[2])
+    tabs.grid(row=0,column=0)
+    uusaken.mainloop()
+
 btn=Button(aken,text="Vajuta siia",font="Arial 20",fg="green",bg="lightblue",width=20, height=3)
+btn2=Button(aken,text="Veel aken",font="Arial 20",fg="green",bg="lightblue", command=uus_aken).pack()
 lbl=Label(aken,text="...")
 ent=Entry(aken,fg="BLue", width=20, font="Arial 20")
 var=IntVar()
